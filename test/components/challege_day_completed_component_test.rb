290fb70ac50a4f8311df5e8009d1061ddc807235
binarygit <inscription4@outlook.com>
2024-05-28 11:34:56 +0545

    Refactor: DRY out rendering of the challenge-day success card

---
+++ test/components/challege_day_completed_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class ChallegeDayCompletedComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(ChallegeDayCompletedComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
