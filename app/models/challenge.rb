412a2e6b9ddd896f08317d3bf0cfc246ae279a6c
binarygit <inscription4@outlook.com>
2024-05-29 12:14:39 +0545

    Create challenge_days after a challenge is created

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,3 +1,13 @@
 class Challenge < ApplicationRecord
   has_many :challenge_days
+
+  after_create :create_challenge_days
+
+  private
+
+  def create_challenge_days
+    (start_date..end_date).each do |date|
+      ChallengeDay.create(date: date, challenge: self)
+    end
+  end
 end
--- test/models/challenge_test.rb
+++ test/models/challenge_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class ChallengeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_creates_challenge_days_after_creation
+    challenge = Challenge.create(name: "new challenge", start_date: Date.new(2024, 05, 01), end_date: Date.new(2024, 05, 31))
+    assert_equal 31, challenge.challenge_days.count
+  end
 end
