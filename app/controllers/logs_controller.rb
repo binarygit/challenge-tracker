1e025ba8cc6b6e94845e4f095574eb78cb0a1fbb
binarygit <inscription4@outlook.com>
2024-05-29 22:05:38 +0545

    Enable commit filtering by date

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,10 +1,16 @@
 class LogsController < ApplicationController
+  include LogsHelper
+
   def index
     @repo = 'https://github.com/binarygit/show-job'
     @commits = File.read('show-job.log').scan(/commit.+\n.+\n.+\n\n.+/)
     @message = "Showing all commits"
   end
 
-  def new
+  def create
+    date = Date.new(params[:date][:year].to_i, params[:date][:month].to_i, params[:date][:day].to_i)
+    @commits = File.read('show-job.log').scan(/commit.+\n.+\n.+\n\n.+/)
+    @commits = commits_on_date(date, @commits)
+    @message = "Showing commits on #{date}"
   end
 end
