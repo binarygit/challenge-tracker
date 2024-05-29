3c2b8d217fc2dc225a4ca6138e06c376f47e2154
binarygit <inscription4@outlook.com>
2024-05-29 21:44:06 +0545

    Create initial logs viewer

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,2 +1,10 @@
 class LogsController < ApplicationController
+  def index
+    @repo = 'https://github.com/binarygit/show-job'
+    @commits = File.read('show-job.log').scan(/commit.+\n.+\n.+\n\n.+/)
+    @message = "Showing all commits"
+  end
+
+  def new
+  end
 end
--- app/helpers/logs_helper.rb
+++ app/helpers/logs_helper.rb
@@ -1,2 +1,25 @@
 module LogsHelper
+  def author(commit)
+    commit.scan(/Author[^>]+>/).first.gsub(/Author: /, '').strip
+  end
+
+  def message(commit)
+    commit.match(/\n\n.+/).to_s.strip
+  end
+
+  def dates(commits)
+    commits.map { Date.parse(_1.match(/Date[^\+]+/).to_s.gsub(/Date:/, '').strip) }.uniq
+  end
+
+  def commits_on_date(date, commits)
+    commits.select do |commit|
+      date == get_date(commit)
+    end
+  end
+
+  private
+
+  def get_date(commit)
+    commit.scan(/Date[^\+]+/).map { Date.parse(_1.gsub(/Date:/, '').strip) }.first
+  end
 end
