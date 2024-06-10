e587c301819344a66237047cb67c344e1870e2c7
binarygit <inscription4@outlook.com>
2024-06-10 12:37:10 +0545

    Change enum name to fail from failure

--- app/models/challenge_day.rb
+++ app/models/challenge_day.rb
@@ -1,4 +1,4 @@
 class ChallengeDay < ApplicationRecord
   belongs_to :challenge
-  enum :status, [ :neutral, :success, :failure ], default: :neutral
+  enum :status, [ :neutral, :success, :fail ], default: :neutral
 end
