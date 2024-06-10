5aec645599470d1315646a13cc5a35cdfbffdf55
binarygit <inscription4@outlook.com>
2024-06-10 12:47:28 +0545

    Display challenge days according to their status

--- app/controllers/challenge_days_controller.rb
+++ app/controllers/challenge_days_controller.rb
@@ -2,7 +2,7 @@ class ChallengeDaysController < ApplicationController
   def update
     @challenge_day = ChallengeDay.find_by id: params[:id]
     if @challenge_day
-      @challenge_day.update photo: params[:photo].read, completed: true
+      @challenge_day.update photo: params[:photo].read, status: params[:status]
     end
   end
 end
