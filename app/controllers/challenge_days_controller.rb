01f51bac4b72b7bbb6c8c4c537406a28a5359b51
binarygit <inscription4@outlook.com>
2024-06-10 23:35:19 +0545

    Attach images as AS records

--- app/controllers/challenge_days_controller.rb
+++ app/controllers/challenge_days_controller.rb
@@ -2,7 +2,9 @@ class ChallengeDaysController < ApplicationController
   def update
     @challenge_day = ChallengeDay.find_by id: params[:id]
     if @challenge_day
-      @challenge_day.update photo: params[:photo].read, status: params[:status]
+      compressed_file = ImageProcessing::MiniMagick.strip.quality(85).interlace('Plane').gaussian_blur('0.05').call(params[:photo])
+      @challenge_day.images.attach(io: compressed_file, filename: params[:photo].original_filename, content_type: 'image/jpeg')
+      @challenge_day.update status: params[:status]
     end
   end
 end
--- app/controllers/challenges/months_controller.rb
+++ app/controllers/challenges/months_controller.rb
@@ -6,6 +6,6 @@ class Challenges::MonthsController < ApplicationController
 
   def show
     @challenge = Challenge.find_by id: params[:challenge_id]
-    @challenge_days = @challenge.challenge_days.group_by {|cd| cd.date.month}[params[:month].to_i]
+    @challenge_days = @challenge.challenge_days.with_attached_images.group_by {|cd| cd.date.month}[params[:month].to_i]
   end
 end
