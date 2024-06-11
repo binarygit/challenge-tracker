0489cea4594899af9c547048b6696418ed471516
binarygit <inscription4@outlook.com>
2024-06-11 19:35:57 +0545

    Create user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_11_124601) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_11_134919) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -53,6 +53,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_11_124601) do
     t.string "name"
     t.date "start_date"
     t.date "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
