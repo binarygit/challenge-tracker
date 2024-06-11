28c7bf2b2cdf3c45f893e21cd7ecfd27157c79f6
binarygit <inscription4@outlook.com>
2024-06-11 11:46:55 +0545

    Change var name

--- app/controllers/challenge_days_controller.rb
+++ app/controllers/challenge_days_controller.rb
@@ -2,8 +2,8 @@ class ChallengeDaysController < ApplicationController
   def update
     @challenge_day = ChallengeDay.find_by id: params[:id]
     if @challenge_day
-      compressed_file = ImageProcessing::MiniMagick.strip.quality(85).interlace('Plane').gaussian_blur('0.05').call(params[:photo])
-      @challenge_day.images.attach(io: compressed_file, filename: params[:photo].original_filename, content_type: 'image/jpeg')
+      compressed_image = ImageProcessing::MiniMagick.strip.quality(85).interlace('Plane').gaussian_blur('0.05').call(params[:photo])
+      @challenge_day.images.attach(io: compressed_image, filename: params[:photo].original_filename, content_type: 'image/jpg')
       @challenge_day.update status: params[:status]
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_10_142315) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_11_060018) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -41,7 +41,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_10_142315) do
 
   create_table "challenge_days", force: :cascade do |t|
     t.date "date"
-    t.binary "photo"
     t.integer "challenge_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
