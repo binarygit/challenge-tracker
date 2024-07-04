7c56b54783de3a84528601ff8755ff26465b4eaf
binarygit <inscription4@outlook.com>
2024-07-04 11:36:34 +0545

    Refactor: Use resource-oriented form_with

--- app/controllers/challenge_days_controller.rb
+++ app/controllers/challenge_days_controller.rb
@@ -2,8 +2,14 @@ class ChallengeDaysController < ApplicationController
   def update
     @challenge_day = ChallengeDay.find_by id: params[:id]
     if @challenge_day
-      @challenge_day.images.attach(params[:photo])
-      @challenge_day.update status: params[:status], note: params[:note]
+      @challenge_day.images.attach(params.dig(:challenge_day, :photo))
+      @challenge_day.update challenge_day_params
     end
+  end
+
+  private
+
+  def challenge_day_params
+    params.require(:challenge_day).permit(:status, :note)
   end
 end
